7f627580bd7422b69caa6ab60a9edb0c354d11a4
vovanovak <2411vova@gmail.com>
2016-05-14 22:05:03 +0300

    Program commit

---
+++ Lab12/Lab12/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdarg.h>
+#include <time.h>
+
+#define RANDOM_RANGE 4
+#define SOME_NUMBER (rand() % RANDOM_RANGE + 1)
+
+using namespace std;
+
+void isDividedOnQ(int n, ...){
+	va_list vl;
+	va_start(vl, n);
+	int q = va_arg(vl, int);
+	
+	cout << "Can be divided on " << q << ": " << endl;
+
+	for (int i = 1; i < n; i++){
+		int val = va_arg(vl, int);
+		cout << val;
+		if (val % q == 0)
+			cout << "(can be divided) ";
+		else
+			cout << " ";
+	}
+
+	cout << endl;
+	va_end(vl);
+}
+
+void main(){
+	srand(time(NULL));
+	
+	cout << "Lab12" << endl;
+	
+	isDividedOnQ(7, SOME_NUMBER, SOME_NUMBER, SOME_NUMBER,
+		SOME_NUMBER, SOME_NUMBER, SOME_NUMBER, SOME_NUMBER);
+}
